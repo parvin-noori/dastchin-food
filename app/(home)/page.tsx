621eebb14d13b1a69a5442c555caf3eab239bdf0
parvin-noori <parvinnoori0@gmail.com>
2026-02-23 14:15:51 +0330

    update home

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,7 +9,10 @@ import Banner from "../_components/banner";
 import Category from "../_components/category";
 import { categoryItem } from "../_components/category/category.types";
 import Products from "../_components/products";
-import { ProductItem, ProductReview } from "../_components/products/product.types";
+import {
+  ProductItem,
+  ProductReview,
+} from "../_components/products/product.types";
 import Slider from "../_components/slider";
 import { sliderItem } from "../_components/slider/slider.types";
 
@@ -19,6 +22,8 @@ export default async function Home() {
   const banners: sliderItem[] = await getBanners();
   const products: ProductItem[] = await getProducts();
   const reviews: ProductReview[] = await getProductReviews();
+  const popularProducts = products.filter((p) => p.is_popular);
+  const newProducts = products.filter((p) => p.is_new);
   return (
     <div className="py-5 gap-5 flex flex-col">
       <div className="grid grid-cols-12 gap-5">
@@ -28,7 +33,16 @@ export default async function Home() {
         <Banner banners={banners} />
       </div>
       <Category categories={categories} />
-      <Products products={products} reviews={reviews} title="پرطرفدارهای فست فود" />
+      <Products
+        products={popularProducts}
+        reviews={reviews}
+        title="پرطرفدارهای فست فود"
+      />
+      <Products
+        products={newProducts}
+        reviews={reviews}
+        title="تخفیف سفارش اول!"
+      />
     </div>
   );
 }
