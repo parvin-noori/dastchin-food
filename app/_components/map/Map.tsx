013234cb5353c5fce24632b091b878be0b704324
parvin-noori <parvinnoori0@gmail.com>
2026-02-24 13:27:55 +0330

    add map component

--- app/_components/map/Map.tsx
+++ app/_components/map/Map.tsx
@@ -1,49 +1,45 @@
-import { Feature, Map } from "@neshan-maps-platform/ol";
-import NeshanMap, {
-  NeshanMapRef,
-} from "@neshan-maps-platform/react-openlayers";
-import { useEffect, useRef } from "react";
+import NeshanMap from "@neshan-maps-platform/react-openlayers";
 
 export default function CustomMap() {
-//   const handleInit = (map: Map) => {
-//     map.setMapType("osm-bright");
-//     map.switchTrafficLayer(true);
-//     // add Marker to the map
-//     const marker = new Feature({
-//       geometry: new Point(
-//         fromLonLat([59.672495, 36.307706]), // [lng, lat]
-//       ),
-//     });
+  //   const handleInit = (map: Map) => {
+  //     map.setMapType("osm-bright");
+  //     map.switchTrafficLayer(true);
+  //     // add Marker to the map
+  //     const marker = new Feature({
+  //       geometry: new Point(
+  //         fromLonLat([59.672495, 36.307706]), // [lng, lat]
+  //       ),
+  //     });
 
-//     marker.setStyle(
-//       new Style({
-//         image: new Icon({
-//           anchor: [0.5, 1],
-//           scale: 0.7,
-//           src: "/marker.png", // داخل public بذار
-//         }),
-//       }),
-//     );
+  //     marker.setStyle(
+  //       new Style({
+  //         image: new Icon({
+  //           anchor: [0.5, 1],
+  //           scale: 0.7,
+  //           src: "/marker.png", // داخل public بذار
+  //         }),
+  //       }),
+  //     );
 
-//     const vectorSource = new VectorSource({
-//       features: [marker],
-//     });
+  //     const vectorSource = new VectorSource({
+  //       features: [marker],
+  //     });
 
-//     const vectorLayer = new VectorLayer({
-//       source: vectorSource,
-//     });
+  //     const vectorLayer = new VectorLayer({
+  //       source: vectorSource,
+  //     });
 
-//     map.addLayer(vectorLayer);
-//   };
+  //     map.addLayer(vectorLayer);
+  //   };
 
-//   useEffect(() => {
-//     if (mapRef.current?.map) {
-//       mapRef.current?.map.switchTrafficLayer(true);
-//       mapRef.current?.map.setMapType("standard-night");
-//     }
-//   }, []);
+  //   useEffect(() => {
+  //     if (mapRef.current?.map) {
+  //       mapRef.current?.map.switchTrafficLayer(true);
+  //       mapRef.current?.map.setMapType("standard-night");
+  //     }
+  //   }, []);
 
-//   const mapRef = useRef<NeshanMapRef | null>(null);
+  //   const mapRef = useRef<NeshanMapRef | null>(null);
   return (
     <NeshanMap
       mapKey="web.3a9b0b3fa3d7401aa946f6f11f09e86e"
@@ -52,7 +48,7 @@ export default function CustomMap() {
       zoom={17}
       poi={true}
       traffic={false}
-    //   onInit={handleInit}
+      //   onInit={handleInit}
     ></NeshanMap>
   );
 }
